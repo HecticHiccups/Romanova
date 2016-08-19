9f8535fbdbb7930565a81222e2f678d19ccac755
Jonathan <Jportorreal77@gmail.com>
2016-08-18 21:44:58 -0400

    used bootstrap to make a stylish menu, centered Romanova speech, adding new sections

--- app/static/index.js
+++ app/static/index.js
@@ -111,3 +111,36 @@ function run(imgurl) {
         
     })
 }
+
+/* Menu Animations */
+
+$(document).ready(function() {
+
+  var active1 = false;
+  var active2 = false;
+  var active3 = false;
+  var active4 = false;
+
+    $('.menu').on('mousedown touchstart', function() {
+    
+    if (!active1) $(this).find('.team').css({'background-color': 'gray', 'transform': 'translate(0px,125px)'});
+    else $(this).find('.team').css({'background-color': 'dimGray', 'transform': 'none'}); 
+
+        if (!active2) $(this).find('.info').css({'background-color': 'gray', 'transform': 'translate(60px,105px)'});
+    else $(this).find('.info').css({'background-color': 'darkGray', 'transform': 'none'});
+
+        if (!active3) $(this).find('.camera').css({'background-color': 'gray', 'transform': 'translate(105px,60px)'});
+    else $(this).find('.camera').css({'background-color': 'silver', 'transform': 'none'});
+
+        if (!active4) $(this).find('.api').css({'background-color': 'gray', 'transform': 'translate(125px,0px)'});
+    else $(this).find('.api').css({'background-color': 'silver', 'transform': 'none'});
+
+    active1 = !active1;
+    active2 = !active2;
+    active3 = !active3;
+    active4 = !active4;
+      
+    });
+});
+
+
